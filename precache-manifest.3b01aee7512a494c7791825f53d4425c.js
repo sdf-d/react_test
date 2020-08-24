7e9bda6c96f15af16b6840d4f7981bf34f552d78
sdger <sddfe@fherh>
2020-08-24 23:34:34 +0300

    Updates

---
+++ precache-manifest.3b01aee7512a494c7791825f53d4425c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e023222b1dfd1216a06f0f7604af690",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "7931b8307c78e4b20e3a",
+    "url": "/react_test/static/css/main.d7ca41e7.chunk.css"
+  },
+  {
+    "revision": "6bf3ab3c83e297b9cf40",
+    "url": "/react_test/static/js/2.691a5a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.691a5a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7931b8307c78e4b20e3a",
+    "url": "/react_test/static/js/main.fe7f9e6c.chunk.js"
+  },
+  {
+    "revision": "40022c35e54cadedd7f8",
+    "url": "/react_test/static/js/runtime-main.435df484.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
